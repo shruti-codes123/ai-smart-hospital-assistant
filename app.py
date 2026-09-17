ae96ff8bb2e29349f6879d041130f5a3f0a34031
Shruti <s71428051@gmail.com>
2026-09-17 21:18:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Hospital@123",
+        password="MY_MYSQL_PASSWORD",",
         database="smart_hospital",
         autocommit=True
     )
